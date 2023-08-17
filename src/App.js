d91d0b64e4ae1ce14603f8c913aa5c95673d90e6
Joceliyn <jocevaldez98@gmail.com>
2023-08-16 22:44:05 -0700

    added to header and footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "boostrap/dist/js/bootstrap.bundle.min.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from 'react';
 
-export default App;
+
