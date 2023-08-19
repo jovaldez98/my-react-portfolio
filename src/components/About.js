9d3a0e085877b279f39f9d8272e8cdf30841726d
Joceliyn <jocevaldez98@gmail.com>
2023-08-18 19:09:46 -0700

    worked on about and contact

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import joceImage from "../assets/images/joceimage.jpg";
+import "./style.css";
+
+export default function About() {
+    return (
+        <>
+        <h4 className="aboutMe">About Joceliyn</h4>
+        <img src={joceImage} alt="Joceliyn Valdez" className="joceImage" />
+
+        <p className="aboutMeParagraph">
+        Joceliyn is a full stack web developer with a unique background. She started her
+        career in the Psychology field. After receiving her Bacherlor's degree in Psychology, she worked 
+        with children with Autism as a Behavior Therapist for a year. It was a very fulfilling job, but she
+        wanted to do more. After making connections with other people in the tech field, she became
+        interested in the world of coding. Joceliyn decided to take a leap of faith she never thought she 
+        would take and enorolled at the UCLA Extension Full Stack Web Development Bootcamp.
+       </p>
+
+       <p className="aboutMeParagraph2">
+        Joceliyn is a very hard worker and is always looking for ways to improve herself.
+        She is a very fast learner and is always up for a challenge, explaining her zero background
+        in coding before enrolling in the bootcamp and now being able to create full stack applications.
+        Joceliyn is very excited to start her career in the tech field and is looking forward to 
+        learning more and adapting to the ever changing world of technology.
+       </p>
+        </>
+    );
+}
---
+++ src/components/Contact.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+// Helper function that checks if an email is valid
+import { validateEmail } from '../utils/helpers';
+
+function Contact() {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        // Conditional statement that checks if there is anything in the input field
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleValidation = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === 'email') {
+            if (!validateEmail(inputValue)) {
+                setErrorMessage("Your email is invalid");
+            } else {
+                if (!inputValue.length) {
+                    setErrorMessage(`${inputType} is required`);
+                } else {
+                    setErrorMessage("");
+                }
+            }
+        };
+
+        const handleFormSubmit = (e) => {
+            e.preventDefault();
+
+            setErrorMessage("Your contact information has been submitted!");
+
+            setEmail("");
+            setMessage("");
+        };
+
+        return (
+            <div className="contactForm">
+                <Form>
+                    <Form.Group className="mb-3" controlId="contactForm.ControlInput1">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email"
+                            placeholder="name@mail.com"
+                            value={email} name="email"
+                            onChange={handleInputChange}
+                            onBlur={handleValidation} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="contactForm.ControlTextarea1">
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control as="textarea"
+                            rows={3}
+                            value={message}
+                            name="message"
+                            onChange={handleInputChange}
+                            onBlur={handleValidation} />
+                    </Form.Group>
+                    <div>
+                        <Button type="submit" className="submitBtn" onClick={handleFormSubmit} variant="outline-dark">Send</Button>
+                    </div>
+                </Form>
+
+                {errorMessage && (
+                    <div>
+                        <p className="errorText">{errorMessage}</p>
+                        </div>
+                )}
+            </div>
+        );
+    }
+}
+
+export default Contact;
