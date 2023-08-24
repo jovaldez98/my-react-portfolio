381c73f3f64e4544a99920af059658bb3216d42f
Joceliyn <jocevaldez98@gmail.com>
2023-08-23 20:57:51 -0700

    fixed the contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -32,56 +32,58 @@ function Contact() {
             if (!validateEmail(inputValue)) {
                 setErrorMessage("Your email is invalid");
             } else {
-                if (!inputValue.length) {
-                    setErrorMessage(`${inputType} is required`);
-                } else {
-                    setErrorMessage("");
-                }
+                setErrorMessage('');
             }
-        };
+        } else {
+            if (!inputValue.length) {
+                setErrorMessage(`${inputType} is required`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+    };
 
-        const handleFormSubmit = (e) => {
-            e.preventDefault();
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
 
-            setErrorMessage("Your contact information has been submitted!");
+        setErrorMessage("Your contact information has been submitted!");
 
-            setEmail("");
-            setMessage("");
-        };
+        setEmail("");
+        setMessage("");
+    };
 
-        return (
-            <div className="contactForm">
-                <Form>
-                    <Form.Group className="mb-3" controlId="contactForm.ControlInput1">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email"
-                            placeholder="name@mail.com"
-                            value={email} name="email"
-                            onChange={handleInputChange}
-                            onBlur={handleValidation} />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="contactForm.ControlTextarea1">
-                        <Form.Label>Message</Form.Label>
-                        <Form.Control as="textarea"
-                            rows={3}
-                            value={message}
-                            name="message"
-                            onChange={handleInputChange}
-                            onBlur={handleValidation} />
-                    </Form.Group>
-                    <div className='submitBtnCenter'>
-                        <Button type="submit" className="submitBtn" onClick={handleFormSubmit} variant="outline-dark">Send</Button>
-                    </div>
-                </Form>
-
-                {errorMessage && (
-                    <div>
-                        <p className="errorText">{errorMessage}</p>
-                        </div>
-                )}
-            </div>
-        );
-    }
+    return (
+        <div className="contactForm">
+            <Form>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email"
+                        placeholder="name@mail.com"
+                        value={email} name="email"
+                        onChange={handleInputChange}
+                        onBlur={handleValidation} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Message:</Form.Label>
+                    <Form.Control as="textarea"
+                        placeholder="Your message here..."
+                        rows={3}
+                        value={message}
+                        name="message"
+                        onChange={handleInputChange}
+                        onBlur={handleValidation} />
+                </Form.Group>
+                <div className='contactBtnCenter'>
+                    <Button type="submit" className="submitBtn" onClick={handleFormSubmit} variant='outline-dark'>Send</Button>
+                </div>
+            </Form>
+            {errorMessage && (
+                <div>
+                    <p className="errorText">{errorMessage}</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Contact;
